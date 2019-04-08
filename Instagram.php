659fee22f0e5fd6f9f802c80fd3d08830f9961e4
Martin Dobry <32479112+dobraczek@users.noreply.github.com>
2019-04-08 21:23:30 +0200

    Update Instagram.php

--- Instagram.php
+++ Instagram.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Instagram Photo
  * @author Martin Dobry
@@ -11,40 +10,39 @@ namespace InstagramPhotos;
 
 class Photos {
 
-    public $Token;
-    public $URI;
+	public $Token;
+	public $URI;
 	
 	public function __construct($ic = null) {
-	    $this->Token = "";
+		$this->Token = "";
 		$this->URI = "https://api.instagram.com/v1/users/self/media/recent/?access_token=".$this->Token;
 	}
 	
 	private function file_get_contents_curl($url) {
-	    $ch = curl_init();
-	    curl_setopt($ch, CURLOPT_HEADER, 0);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    curl_setopt($ch, CURLOPT_URL, $url);
-	    $data = curl_exec($ch);
-	    curl_close($ch);
-	    return $data;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 	}
 	
 	private function echoImage($value) {
-        $link = $value["link"];
-        $thumb = $value["images"]["standard_resolution"]["url"];
-        return '<a href="'.$link.'"><img src="'.$thumb.'" alt="" width="100%" /></a>';
-    }
+		$link = $value["link"];
+		$thumb = $value["images"]["standard_resolution"]["url"];
+		return '<a href="'.$link.'"><img src="'.$thumb.'" alt="" width="100%" /></a>';
+	}
 	
 	public function Images() {
-	    $result = $this->file_get_contents_curl($this->URI);
+		$result = $this->file_get_contents_curl($this->URI);
 	    
-	    $json = json_decode($result, 1);
+		$json = json_decode($result, 1);
 	    
-	    foreach ($json['data'] as $value)
-	        $photo[] = $this->echoImage($value);
+		foreach ($json['data'] as $value)
+			$photo[] = $this->echoImage($value);
 	    
-        return json_encode($photo);
-        
+		return json_encode($photo);
 	}
 	
 }
