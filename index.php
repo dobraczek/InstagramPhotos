018d4912f8d84b7488d311024fa5b8a3987239d3
Martin Dobry <32479112+dobraczek@users.noreply.github.com>
2019-04-08 21:53:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
 
 <head>
 	<meta charset="utf-8">
-	<title>Instagram Photo</title>
-	
+	<title>Instagram Photos</title>
+
 	<meta content="" name="keywords">
 	<meta content="" name="author">
-	
+
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-	
+
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
@@ -21,28 +21,28 @@
 
 <div class="container">
 
-	<h1>Instagram Photo</h1>
+	<h1>Instagram Photos</h1>
 
-    <div class="row">
-        <?php
-        /**
-         * Instagram Photo
-         * @author Martin Dobry
-         * @link http://webscript.cz
-         * @version 1.0
-         */
-        
-        include "Instagram.php";
-        $Instagram = new InstagramPhotos\Photos();
-        $json = $Instagram->Images();
-        
-        $array = json_decode($json, 1);
-        
-        foreach ($array as $data)
-            echo '<div class="col-md-2 margin-bottom-30">'.$data.'</div>';
-        
-        ?>
-    </div>
+	<div class="row">
+	<?php
+	/**
+	 * Instagram Photos
+	 * @author Martin Dobry
+	 * @link http://webscript.cz
+	 * @version 1.0
+	 */
+
+	include "Instagram.php";
+	$Instagram = new InstagramPhotos\Photos();
+	$json = $Instagram->Images();
+
+	$array = json_decode($json, 1);
+
+	foreach ($array as $data)
+		echo '<div class="col-md-2 margin-bottom-30">'.$data.'</div>';
+
+	?>
+	</div>
 </div>
 
 </body>
